5c4d0886d08286aaf7b921e21e7aa6e8b3c3e258
dereje <getutadessedereje@icloud.com>
2025-07-03 18:48:21 +0200

    Refactor main function to initialize server with Axum and custom configurations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+mod handlers;
+mod types;
+mod utils;
+mod config;
+
+use std::net::SocketAddr;
+use config::create_app;
+
+#[tokio::main]
+async fn main() {
+    let app = create_app();
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    
+    println!("Shoot for the Stars, Aim for the Moon");
+    println!("📡 Listening on: http://{}", addr);
+    println!("...Ready for it?");
+    
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
